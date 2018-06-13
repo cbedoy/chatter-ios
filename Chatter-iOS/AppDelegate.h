69ac4668400594044d8f751d0c243b845e3949af
Bedoy <carlos.bedoy@gmail.com>
2018-06-13 10:26:45 -0500

    Initial Commit

---
+++ Chatter-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chatter-iOS
+//
+//  Created by Carlos Bedoy on 6/13/18.
+//  Copyright © 2018 Carlos Bedoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chatter-iOSTests/Chatter_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  Chatter_iOSTests.m
+//  Chatter-iOSTests
+//
+//  Created by Carlos Bedoy on 6/13/18.
+//  Copyright © 2018 Carlos Bedoy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Chatter_iOSTests : XCTestCase
+
+@end
+
+@implementation Chatter_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
